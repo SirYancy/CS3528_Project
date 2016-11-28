24fa867fbb203dbf1c686cc47f72469e44ce60f3
Eric Kuha <erickuha@gmail.com>
2016-11-28 17:17:53 -0600

    warehouse header file. Rough sketch

--- Warehouse.h
+++ Warehouse.h
@@ -3,6 +3,9 @@
 
 #include <string>
 #include <vector>
+#include <unordered_map>
+#include <iostream>
+#include <iomanip>
 #include "Client.h"
 #include "Package.h"
 #include "Truck.h"
@@ -14,9 +17,54 @@ class Truck
 
 //! Warehouse Class
 /*!
- * This Warehouse class defines a warehouse that 
+ * This Warehouse class defines a warehouse that will keep track of the all clients, packages, and trucks. It will also generate the routes for all the trucks and load the trucks with packages.
+ */
 class Warehouse
 {
+    public:
+        //! Basic constructor builds an empty warehouse
+        Warehouse();
+
+        //! Returns the map of all clients in the system
+        unordered_map<string, Client*> getClients() const;
+
+        //! Returns a vector of all currently undelivered packages
+        vector<Package*> getUndelivered() const;
+
+        //! Returns a vector of all packages previously delivered
+        vector<Package*> getDelivered() const;
+
+        //! Returns a vector of all packages that have been loaded onto a truck
+        vector<Package*> getLoaded() const;
+
+        //! Returns a vector of all trucks in the fleet
+        vector<Truck*> getTrucks() const;
+
+        //! Creates a new truck, adds it to the fleet, and then returns a pointer to that truck.
+        Truck* makeTruck() const;
+
+        //! Adds a new undelivered package to the warehouse
+        void addPackage(const Package&);
+
+        //! Adds a new client to the system
+        void addClient(const Client&);
+
+        
 
 
+    private:
+        //! Map of clients
+        unordered_map<string, Client*> clients;
+        //! Vector of undelivered packages
+        vector<Package*> undeliveredPackages;
+        //! Vector of packages currently loaded on trucks
+        vector<Package*> loadedPackages;
+        //! Vector of packages that are delivered
+        vector<Package*> deliveredPackages;
+        //! Vector of trucks
+        vector<Truck*> trucks;
 
+
+};
+
+#endif //WARHOUSE_H
